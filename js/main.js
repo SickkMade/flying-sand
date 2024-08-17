c6e8abd400d79e446a1958ad2ac36d580bce3158
sickk <jaakmyr@gmail.com>
2024-08-16 22:47:28 -0700

    now bouncy

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 let ball = document.querySelector('.ball')
 let ballpit = document.querySelector('#ballpit')
 
-let balls = []
+ball.remove()
+
 
 document.addEventListener('mousemove', event => {
     if(event.buttons == 1){ //if mouse down
@@ -9,28 +10,47 @@ document.addEventListener('mousemove', event => {
             var randomColor = Math.floor(Math.random()*16777215).toString(16);
             let newBall = ball.cloneNode()
             newBall.style.backgroundColor = "#" + randomColor
+            newBall.setAttribute('data-wind',Math.random()*10 - 5)
             newBall.style.left = event.clientX+'px'
             newBall.style.top = event.clientY+'px'
             ballpit.appendChild(newBall);
-            balls.push(newBall)
         })
     }
 })
 
 function fall(){
-    console.log(window.innerWidth)
+    let balls = document.querySelectorAll('.moving-ball')
     balls.forEach(ball => {
-        if(parseInt(ball.style.top) < window.innerHeight-25){
-            ball.style.top = Math.random()*5 + parseInt(ball.style.top) + 'px';
-            ball.style.left = Math.random()*5 + parseInt(ball.style.left) + 'px';
+        let currentTop = parseInt(ball.style.top);
+        let currentLeft = parseInt(ball.style.left);
+        let velocity = parseFloat(ball.getAttribute('data-velocity'));
+        let ballWind = parseInt(ball.getAttribute('data-wind'));
+        
+        velocity +=.025
+        ball.style.top = (velocity + currentTop) + 'px';
+        ball.setAttribute('data-velocity', velocity);
+
+        //wind
+        ball.style.left = Math.random() * ballWind + currentLeft + 'px';
+
+        if(currentTop > window.innerHeight - 25 && velocity > 0){
+            if(velocity <= 1.5){
+                ball.classList.remove('moving-ball')
+                setTimeout(() => ball.remove(), 1000)
+            } 
+
+            ball.setAttribute('data-velocity', velocity * -Math.random() * 0.65);
         }
-        else{
-            ball.style.top = 0
+        //left wall
+        if(currentLeft < 0 && ballWind < 0){   
+            ball.setAttribute('data-wind', -ballWind)
         }
-        if(parseInt(ball.style.left) > window.innerWidth) {
-            ball.style.left = 0
+        //walls
+        else if(currentLeft > window.innerWidth && ballWind > 0){
+            ball.setAttribute('data-wind', -ballWind)
         }
-    })
+    });
 }
+
 
 setInterval(() => requestAnimationFrame(fall), 1)
